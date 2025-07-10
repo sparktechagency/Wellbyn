aec80878afe629a50c1478d7c1becb283b865357
Rosdeb <rosdekoc12@gmail.com>
2025-07-11 04:56:22 +0600

    animation splash screen

---
+++ lib/views/screen/splash_screen_animation/animatedSplashScreen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:get/get.dart';
+import '../auth/create_acount_screen.dart';
+
+class AnimatedSplashScreen extends StatefulWidget {
+  const AnimatedSplashScreen({super.key});
+
+  @override
+  State<AnimatedSplashScreen> createState() => _AnimatedSplashScreenState();
+}
+
+class _AnimatedSplashScreenState extends State<AnimatedSplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Lottie.asset(
+          'assets/animations/Success_Check.json',
+          onLoaded: (composition) {
+            Future.delayed(composition.duration, () {
+              Get.offAll(() => CreateAccountPage(role: "doctor"));
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
